4dc92b5f625d94faaee9bd78d2d2e7b910cb6ae8
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-02-10 11:03:48 +0100

    create server

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+import errorsHandler from "./middlewares/errorsHandler.js";
+import notFound from "./middlewares/notFound.js";
+import corsPolicy from './middlewares/corsPolicy.js';
+import router from "./routers/properties.js";
+
+app.use(express.static("public"));
+
+app.use(corsPolicy);
+
+app.use(express.json());
+
+app.use("/properties", router);
+
+app.use(errorsHandler);
+
+app.use(notFound);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
