2667e4830db9c2eca5bbd64952b2385d8313869f
Melrizzuto <melrizzuto4@gmail.com>
2025-07-05 00:24:25 +0200

    fix

--- connection.js
+++ connection.js
@@ -1,18 +1,14 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-const connection = mysql.createConnection({
+dotenv.config();
+
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.PORT,
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Errore di connessione al database:', err.message);
-        return;
-    }
-    console.log('Connesso al database!');
-});
-
-export default connection;
+export default pool;
