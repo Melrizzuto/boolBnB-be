68e21dc08f4de7e98927e6af8d08b7d7a06bbe6a
Melrizzuto <melrizzuto4@gmail.com>
2025-07-05 00:20:33 +0200

    fix

--- connection.js
+++ connection.js
@@ -1,13 +1,18 @@
 import mysql from "mysql2";
 
-const pool = mysql.createPool({
-    host: 'yamanote.proxy.rlwy.net',
-    user: 'root',
-    password: 'qFPecktzMTmerQhCVRWgIUPHJtxbulMP',
-    database: 'boolbnb_db',
-    port: '31620'
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
-const connection = pool.promise();
+connection.connect((err) => {
+    if (err) {
+        console.error('Errore di connessione al database:', err.message);
+        return;
+    }
+    console.log('Connesso al database!');
+});
 
 export default connection;
